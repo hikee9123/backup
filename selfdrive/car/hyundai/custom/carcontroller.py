e6f7487111340398e8ef52af2268f17b07bb4399
hikee9123 <hikee9123@gmail.com>
2023-12-17 09:46:46 +0900

    fix

--- selfdrive/car/hyundai/custom/carcontroller.py
+++ selfdrive/car/hyundai/custom/carcontroller.py
@@ -8,15 +8,15 @@ from openpilot.selfdrive.car.hyundai.custom.navicontrol  import NaviControl
 class CarControllerCustom:
   def __init__(self, CP):
     self.CP = CP
-    self.resume_cnt = 0    
+    #self.resume_cnt = 0    
     self.NC = NaviControl( CP)
 
   def create_button_messages(self, CC, CS, can_sends):
     # pass
     btn_signal = self.NC.update( CC, CS, CC.frame )
     if btn_signal != None:
-      can_sends.append(hyundaican.create_clu11( CC.packer, CC.resume_cnt, CS.clu11, btn_signal, self.CP.carFingerprint))
-      self.resume_cnt += 1
-    else:
-      self.resume_cnt = 0
+      can_sends.append(hyundaican.create_clu11( CC.packer, CC.frame, CS.clu11, btn_signal, self.CP.carFingerprint))
+      #self.resume_cnt += 1
+    #else:
+      #self.resume_cnt = 0
 
