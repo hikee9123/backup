50b5f60d2bd18ab817a06e88f305bcaa072f2eec
hikee9123 <hikee9123@gmail.com>
2023-12-18 13:09:40 +0900

    fix

--- selfdrive/ui/qt/custom/custom.cc
+++ selfdrive/ui/qt/custom/custom.cc
@@ -101,7 +101,7 @@ CustomPanel::CustomPanel(SettingsWindow *parent) : ListWidget(parent) {
     // 전체 레이아웃을 설정
     //setLayout(mainLayout);
 
-    setLayout(panel_widget);    
+    //setLayout(panel_widget);    
 
     // Set the current page
     panel_widget->setCurrentIndex(0);
