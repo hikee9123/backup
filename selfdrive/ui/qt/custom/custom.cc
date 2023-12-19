d5fda9f683d3c61b0ce7234b1e643e9e3b5c1e50
hikee9123 <hikee9123@gmail.com>
2023-12-19 20:55:05 +0900

    fix

--- selfdrive/ui/qt/custom/custom.cc
+++ selfdrive/ui/qt/custom/custom.cc
@@ -87,7 +87,7 @@ CustomPanel::CustomPanel(SettingsWindow *parent) : QWidget(parent) {
     mainLayout->addWidget(tabWidget);
 
     //tabWidget->move(100, 50);
-    resize(1024, 900);
+   // resize(1024, 900);
    // setLayout(mainLayout);
     //show();
 
