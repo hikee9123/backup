b42a8db6877939e982a3f41732e03d8c34ce1801
hikee9123 <hikee9123@gmail.com>
2023-12-19 20:09:27 +0900

    fix

--- selfdrive/ui/qt/custom/custom.cc
+++ selfdrive/ui/qt/custom/custom.cc
@@ -69,7 +69,7 @@ CustomPanel::CustomPanel(SettingsWindow *parent) : ListWidget(parent) {
     verticalLayout->addWidget(button2);
 
     setLayout(verticalLayout);
-*/
+
     setStyleSheet(R"(
         * {
         color: white;
@@ -83,9 +83,39 @@ CustomPanel::CustomPanel(SettingsWindow *parent) : ListWidget(parent) {
         border-radius: 30px;
         }
     )");
+*/
 
+   // param, title, desc, icon
+  std::vector<std::tuple<QString, QString, QString, QString>> toggle_defs{
+    {
+      "HapticFeedbackWhenSpeedCamera",
+      tr("Haptic feedback (speed-cam alert)"),
+      tr("Haptic feedback when a speed camera is detected"),
+      "../assets/offroad/icon_openpilot.png",
+    },
+    {
+      "UseExternalNaviRoutes",
+      tr("Use external navi routes"),
+      "",
+      "../assets/offroad/icon_openpilot.png",
+    },
+    {
+      "ShowDebugMessage",
+      tr("Show Debug Message"),
+      "",
+      "../assets/offroad/icon_shell.png",
+    },
+  };
 
-    
+  for (auto &[param, title, desc, icon] : toggle_defs) {
+    auto toggle = new ParamControl(param, title, desc, icon, this);
+
+    bool locked = params.getBool((param + "Lock").toStdString());
+    toggle->setEnabled(!locked);
+
+    addItem(toggle);
+    toggles[param.toStdString()] = toggle;
+  }   
 }
 
 
