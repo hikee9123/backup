74a3e788735601bb2f81b10a021af048c94330e9
hikee9123 <hikee9123@gmail.com>
2023-12-23 10:39:06 +0900

    fix

--- selfdrive/ui/qt/custom/custom.cc
+++ selfdrive/ui/qt/custom/custom.cc
@@ -256,6 +256,7 @@ void CommunityPanel::hideEvent(QHideEvent *event)
 {
   printf("CommunityPanel::hideEvent \n" );
   
+  /*
   int HapticFeedbackWhenSpeedCamera = std::atoi(params.get("HapticFeedbackWhenSpeedCamera").c_str());
   int UseExternalNaviRoutes = std::atoi(params.get("UseExternalNaviRoutes").c_str());
   int ShowDebugMessage = std::atoi(params.get("ShowDebugMessage").c_str());
@@ -272,7 +273,7 @@ void CommunityPanel::hideEvent(QHideEvent *event)
   pm->send("uICustom", msg);
   m_cmdIdx++;
 
-/*
+
   // JSON 객체 초기화
   json11::Json::object log_j = json11::Json::object {
       {"HapticFeedbackWhenSpeedCamera", HapticFeedbackWhenSpeedCamera},
