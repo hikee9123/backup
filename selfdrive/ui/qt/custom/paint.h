efbc798ce76b6f281b24df751d3ddabc9043bdcd
hikee9123 <hikee9123@gmail.com>
2023-12-31 13:20:01 +0900

    fix

--- selfdrive/ui/qt/custom/paint.h
+++ selfdrive/ui/qt/custom/paint.h
@@ -46,7 +46,7 @@ private:
 
     cereal::CarStateCustom::Tpms::Reader tpmsData;
     cereal::UICustom::Community::Reader community;
-    cereal::UICustom::Ui::Reader ui;
+    cereal::UICustom::UserInterface::Reader ui;
 
     int   cpuPerc;
     float cpuTemp; 
