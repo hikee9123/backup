e28b6136190fbca3152b9f3ae86604dd857c9d5d
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2024-01-02 19:00:59 +0900

    Update navicontrol.py

--- selfdrive/car/hyundai/custom/navicontrol.py
+++ selfdrive/car/hyundai/custom/navicontrol.py
@@ -113,14 +113,14 @@ class NaviControl():
       if self.target_speed == self.VSetDis:
         self.btn_cnt = 0
         self.seq_command = 3            
-      elif self.btn_cnt > 10:
+      elif self.btn_cnt > 30:
         self.btn_cnt = 0
         self.seq_command = 3
       return Buttons.SET_DECEL
 
   def case_3(self, CS):  # None
       self.btn_cnt += 1
-      if self.btn_cnt > 6: 
+      if self.btn_cnt > 60: 
         self.seq_command = 0
       return None
 
