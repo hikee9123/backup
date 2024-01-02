b6072dfb278704d0531635bbd429bbd2359489b2
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2024-01-02 18:09:25 +0900

    Update carcontroller.py

--- selfdrive/car/hyundai/custom/carcontroller.py
+++ selfdrive/car/hyundai/custom/carcontroller.py
@@ -15,7 +15,7 @@ class CarControllerCustom:
   def create_button_messages( self, can_sends, CC, CS ):
     btn_signal = self.NC.update( CC, CS, CC.frame )
     if btn_signal != None:
-      can_sends = hyundaican.create_clu11(CC.packer, self.resume_cnt, CS.clu11, btn_signal, self.CP.carFingerprint)
+      can_sends.extend( hyundaican.create_clu11(CC.packer, self.resume_cnt, CS.clu11, btn_signal, self.CP.carFingerprint) )
       self.resume_cnt += 1
     else:
       self.resume_cnt = 0
