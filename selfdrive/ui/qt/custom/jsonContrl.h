7f0426348057ded344eb2a08f0a3808f0c6cc5a0
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2024-01-04 14:58:54 +0900

    Update jsonContrl.h

--- selfdrive/ui/qt/custom/jsonContrl.h
+++ selfdrive/ui/qt/custom/jsonContrl.h
@@ -21,8 +21,7 @@ class JsonControl : public ToggleControl {
 
 public:
   JsonControl(const QString &param, const QString &title, const QString &desc, const QString &icon, QWidget *parent, QJsonObject &jsonobj)
-    : ToggleControl(title, desc, icon, false, parent), m_jsonobj(jsonobj)
-    {
+             : ToggleControl(title, desc, icon, false, parent), m_jsonobj(jsonobj) {
     key = param;//.toStdString();
     QObject::connect(this, &JsonControl::toggleFlipped, [=](bool state) {
       QString content("<body><h2 style=\"text-align: center;\">" + title + "</h2><br>"
