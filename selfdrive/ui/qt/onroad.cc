55fc175d1998b097202a0bf3f4f318b9bc318bf4
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2024-01-10 18:10:24 +0900

    Update onroad.cc

--- selfdrive/ui/qt/onroad.cc
+++ selfdrive/ui/qt/onroad.cc
@@ -226,7 +226,7 @@ ExperimentalButton::ExperimentalButton(QWidget *parent) : experimental_mode(fals
 }
 
 void ExperimentalButton::changeMode() {
-  const auto cp = (*uiState()->sm)["carParams"].getCarParams();
+  //const auto cp = (*uiState()->sm)["carParams"].getCarParams();
   //bool can_change = hasLongitudinalControl(cp) && params.getBool("ExperimentalModeConfirmed");
   bool can_change =  params.getBool("ExperimentalModeConfirmed");
   if (can_change) {
