38ae44bebefa891bfd4ea124c0d800e582c4f648
hikee9123 <hikee9123@gmail.com>
2024-01-11 12:48:22 +0900

    fix

--- selfdrive/ui/qt/custom/custom.cc
+++ selfdrive/ui/qt/custom/custom.cc
@@ -416,8 +416,8 @@ void CommunityTab::showEvent(QShowEvent *event)
 
 void CommunityTab::hideEvent(QHideEvent *event)
 {
-  for (auto value : m_valueCtrl) {
-      m_jsonobj[ value.m_params ] = value.getValue();
+  for (auto &value : m_valueCtrl) {
+      m_jsonobj[ value->m_params ] = value->getValue();
   }
 
   //m_jsonobj["CruiseMode"] = m_valueCtrl["CruiseMode"]->getValue();
