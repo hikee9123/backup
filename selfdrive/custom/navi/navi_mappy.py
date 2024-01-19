577cc7396ff830e2aa07ba7b66037699ab21c19f
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2024-01-19 20:09:13 +0900

    Update navi_mappy.py

--- selfdrive/custom/navi/navi_mappy.py
+++ selfdrive/custom/navi/navi_mappy.py
@@ -163,10 +163,10 @@ class MappyServer:
             else:
                 self.dEventHideSec = 7
 
-            if dEventLastSec > self.dEventHideSec:
-                self.speedLimit = 0
-            elif dArrivalTimeSec < 1.5:
-                self.speedLimit = 0
+            #if dEventLastSec > self.dEventHideSec:
+            #    self.speedLimit = 0
+            #elif dArrivalTimeSec < 1.5:
+            #    self.speedLimit = 0
         else:
             self.dHideTimeSec =  self.current_time_seconds + 5
 
