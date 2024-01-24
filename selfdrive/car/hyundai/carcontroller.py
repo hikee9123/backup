d495a9da90b703d83dbd3da9a421432318efc7e8
hikee9123 <hikee9123@gmail.com>
2024-01-24 16:25:03 +0900

    test

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -149,7 +149,6 @@ class CarController:
                                                 hud_control, 
                                                 left_lane_warning, right_lane_warning) )
 
-
       if not self.CP.openpilotLongitudinalControl:
         can_sends.extend( self.customCC.create_button_messages(CC, CS, self.frame ) ) #custom
         #can_sends.extend(self.create_button_messages(CC, CS, use_clu11=True))
