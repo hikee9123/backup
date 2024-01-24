f606484407b6b4c997c874328760888748b71d06
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2024-01-24 12:44:59 +0900

    Update carcontroller.py

--- selfdrive/car/hyundai/custom/carcontroller.py
+++ selfdrive/car/hyundai/custom/carcontroller.py
@@ -43,7 +43,7 @@ class CarControllerCustom:
       return
 
     # 100 Hz
-    can_sends.append( create_mdps12( packer, frame, CS.customCS.mdps12 ) )  # send mdps12 to LKAS to prevent LKAS error
+    #can_sends.append( create_mdps12( packer, frame, CS.customCS.mdps12 ) )  # send mdps12 to LKAS to prevent LKAS error
 
     # 20 Hz LFA MFA message
     if frame % 5 == 0 and self.CP.flags & HyundaiFlags.SEND_LFA.value:
