e0e0a75f3d9acd0196bd6aab3b581bd10e4934ee
hikee9123 <hikee9123@gmail.com>
2024-02-01 11:38:20 +0900

    test

--- selfdrive/car/hyundai/custom/carcontroller.py
+++ selfdrive/car/hyundai/custom/carcontroller.py
@@ -88,8 +88,10 @@ class CarControllerCustom:
 
  
       if not self.CP.openpilotLongitudinalControl:
-        # 100 Hz
-        can_sends.append( create_mdps12( packer, frame, CS.customCS.mdps12 ) )  # send mdps12 to LKAS to prevent LKAS error          
+        if self.car_fingerprint in (CAR.AZERA_HEV_6TH_GEN, CAR.GENESIS_G90):
+          # 100 Hz
+          can_sends.append( create_mdps12( packer, frame, CS.customCS.mdps12 ) )  # send mdps12 to LKAS to prevent LKAS error
+
         can_sends.extend( self.create_button_messages( packer, CC, CS, frame ) ) #custom
 
   
