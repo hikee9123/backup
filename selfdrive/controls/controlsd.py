181b2f401f9f50fd996cf6370c9806e64516a79d
hikee9123 <77569237+hikee9123@users.noreply.github.com>
2024-02-12 19:38:08 +0900

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -473,6 +473,7 @@ class Controls:
     if self.enabled and any(not ps.controlsAllowed for ps in self.sm['pandaStates']
            if ps.safetyModel not in IGNORED_SAFETY_MODES):
       self.mismatch_counter += 1
+      print('enable={} {}'.format( self.enabled,  ps.controlsAllowed ) )
 
     self.distance_traveled += CS.vEgo * DT_CTRL
 
@@ -775,7 +776,6 @@ class Controls:
     steer_angle_without_offset = math.radians(CS.steeringAngleDeg - lp.angleOffsetDeg)
     curvature = -self.VM.calc_curvature(steer_angle_without_offset, CS.vEgo, lp.roll)
 
-    print('enable={}'.format( self.enabled ) )
     # controlsState
     dat = messaging.new_message('controlsState')
     dat.valid = CS.canValid
